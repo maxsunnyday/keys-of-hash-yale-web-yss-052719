028ad2f7d8681502f78322cbee522cf3083daae3
Max Sun <max.sun@yale.edu>
2019-05-18 17:58:59 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -2,9 +2,6 @@ class Hash
   def keys_of(*arguments)
     array = []
     arguments.each do |argument|
-      if value == argument
-        array << key
-      end
       @.each do |key, value|
         if value == argument
           array << key
